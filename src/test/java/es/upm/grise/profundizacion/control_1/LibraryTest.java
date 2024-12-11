21cea2e46c7f7ed42a42724d67d1dbb047d61597
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-12-11 17:51:55 +0100

    Pregunta 4a: addBook

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,52 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class LibraryTest {
+	private Library l;
+
+	@BeforeEach
+	public void setUp() {
+        l = new Library();
+    }
 	
 	@Test
-	public void test() {
-		fail();
+	public void addBookA() {
+		Book b1 = new Book("a");
+		try {
+			l.addBook(b1);
+		} catch (DuplicatedBookException e) {
+			e.printStackTrace();
+		}
+
+		Book bc = null;
+		try {
+			bc = l.getBook("a");
+		} catch (NonExistingBookException e) {
+			e.printStackTrace();
+		} catch (EmptyLibraryException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(b1, bc);
+	}
+
+	@Test
+	public void addBookB() {
+		Book b1 = new Book("a");
+		try {
+			l.addBook(b1);
+		} catch (DuplicatedBookException e) {
+			e.printStackTrace();
+		}
+
+		assertThrows(DuplicatedBookException.class, () -> {
+			l.addBook(b1);
+		});
 	}
 
 }
