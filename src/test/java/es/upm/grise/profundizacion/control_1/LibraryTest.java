2ca782d24cb2f31a52c01cf92f298f306c16e357
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-12-11 18:09:23 +0100

    Pregunta 6: getBook

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -83,4 +83,52 @@ public class LibraryTest {
 		});
 	}
 
+	@Test
+	public void getBook() {
+		Book b1 = new Book("a");
+		Book b2 = new Book("b");
+		try {
+			l.addBook(b1);
+			l.addBook(b2);
+		} catch (DuplicatedBookException e) {
+			e.printStackTrace();
+		}
+
+		Book bc = null;
+		try {
+			bc = l.getBook("a");
+		} catch (NonExistingBookException e) {
+			e.printStackTrace();
+		} catch (EmptyLibraryException e) {
+			e.printStackTrace();
+		}
+		assertEquals(b1, bc);
+
+		try {
+			bc = l.getBook("b");
+		} catch (NonExistingBookException e) {
+			e.printStackTrace();
+		} catch (EmptyLibraryException e) {
+			e.printStackTrace();
+		}
+		assertEquals(b2, bc);
+	}
+
+	@Test
+	public void getBookE() {
+		assertThrows(EmptyLibraryException.class, () -> {
+			l.getBook("a");
+		});
+		Book b1 = new Book("a");
+		try {
+			l.addBook(b1);
+		} catch (DuplicatedBookException e) {
+			e.printStackTrace();
+		}
+
+		assertThrows(NonExistingBookException.class, () -> {
+			l.getBook("b");
+		});
+	}
+
 }
